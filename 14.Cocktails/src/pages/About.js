b15f05263e3a273148d3f2cdb7422305d4e04557
apsmj23 <apurvalion@gmail.com>
2021-05-17 17:55:39 +0530

    Added Cocktails App

---
+++ 14.Cocktails/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className='section-title'>About Us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, libero minus. Exercitationem soluta libero aspernatur. Delectus blanditiis in natus ipsam. Culpa minima quos ratione? Adipisci consequuntur perferendis explicabo consequatur, doloremque quibusdam blanditiis harum eius maxime distinctio molestiae natus voluptatum, odit saepe quisquam hic. Voluptate odit repellat eius consequatur amet itaque!</p>
+    </section>
+  )
+}
+
+export default About
